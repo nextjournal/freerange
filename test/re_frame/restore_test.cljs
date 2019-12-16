383f5305f079e14a3eb2f46276fd7c0146127804
Arne Brasseur <arne@arnebrasseur.net>
2019-12-16 15:15:54 +0100

    Rename the-frame to default-frame, fix reg-sub-raw
    At the top level (re-frame.core/reg-sub-raw), keep the existing API where the
    handler-fn receives the app-db, even though a "raw" invocation in our case would
    receive the frame as its first argument.
    
    If you want to receive the frame then bypass the wrapper added by the top-level
    API by invoking `re-frame.frame/reg-sub-raw` directly.

--- test/re_frame/restore_test.cljs
+++ test/re_frame/restore_test.cljs
@@ -5,7 +5,7 @@
 
 ;; TODO: future tests in this area could check DB state and registrations are being correctly restored.
 
-(use-fixtures :each {:before (partial subs/clear-all-handlers! core/the-frame)})
+(use-fixtures :each {:before (partial subs/clear-all-handlers! core/default-frame)})
 
 (defn one? [x] (= 1 x))
 (defn two? [x] (= 2 x))
@@ -24,51 +24,51 @@
 (deftest make-restore-fn-test
   (testing "no existing subs, then making one subscription"
     (register-test-subs)
-    (let [original-subs @(:subs-cache core/the-frame)
+    (let [original-subs @(:subs-cache core/default-frame)
           restore-fn    (make-restore-fn)]
       (is (zero? (count original-subs)))
       @(subscribe [:test-sub])
-      (is (one? (count @(:subs-cache core/the-frame))))
-      (is (contains? @(:subs-cache core/the-frame) [[:test-sub] []]))
+      (is (one? (count @(:subs-cache core/default-frame))))
+      (is (contains? @(:subs-cache core/default-frame) [[:test-sub] []]))
       (restore-fn)
-      (is (zero? (count @(:subs-cache core/the-frame)))))))
+      (is (zero? (count @(:subs-cache core/default-frame)))))))
 
 (deftest make-restore-fn-test2
   (testing "existing subs, making more subscriptions"
     (register-test-subs)
     @(subscribe [:test-sub])
-    (let [original-subs @(:subs-cache core/the-frame)
+    (let [original-subs @(:subs-cache core/default-frame)
           restore-fn    (make-restore-fn)]
       (is (one? (count original-subs)))
       @(subscribe [:test-sub2])
-      (is (contains? @(:subs-cache core/the-frame) [[:test-sub2] []]))
-      (is (two? (count @(:subs-cache core/the-frame))))
+      (is (contains? @(:subs-cache core/default-frame) [[:test-sub2] []]))
+      (is (two? (count @(:subs-cache core/default-frame))))
       (restore-fn)
-      (is (not (contains? @(:subs-cache core/the-frame) [[:test-sub2] []])))
-      (is (one? (count @(:subs-cache core/the-frame)))))))
+      (is (not (contains? @(:subs-cache core/default-frame) [[:test-sub2] []])))
+      (is (one? (count @(:subs-cache core/default-frame)))))))
 
 (deftest make-restore-fn-test3
   (testing "existing subs, making more subscriptions with different params on same subscriptions"
     (register-test-subs)
     @(subscribe [:test-sub])
-    (let [original-subs @(:subs-cache core/the-frame)
+    (let [original-subs @(:subs-cache core/default-frame)
           restore-fn    (make-restore-fn)]
       (is (one? (count original-subs)))
       @(subscribe [:test-sub :extra :params])
-      (is (two? (count @(:subs-cache core/the-frame))))
+      (is (two? (count @(:subs-cache core/default-frame))))
       (restore-fn)
-      (is (one? (count @(:subs-cache core/the-frame)))))))
+      (is (one? (count @(:subs-cache core/default-frame)))))))
 
 (deftest nested-restores
   (testing "running nested restores"
     (register-test-subs)
     (let [restore-fn-1 (make-restore-fn)
           _            @(subscribe [:test-sub])
-          _            (is (one? (count @(:subs-cache core/the-frame))))
+          _            (is (one? (count @(:subs-cache core/default-frame))))
           restore-fn-2 (make-restore-fn)]
       @(subscribe [:test-sub2])
-      (is (two? (count @(:subs-cache core/the-frame))))
+      (is (two? (count @(:subs-cache core/default-frame))))
       (restore-fn-2)
-      (is (one? (count @(:subs-cache core/the-frame))))
+      (is (one? (count @(:subs-cache core/default-frame))))
       (restore-fn-1)
-      (is (zero? (count @(:subs-cache core/the-frame)))))))
+      (is (zero? (count @(:subs-cache core/default-frame)))))))
